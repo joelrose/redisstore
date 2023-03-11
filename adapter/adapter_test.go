e3090c51be2a9b53bffc44dbe363f0b92edc16d1
Joel Rose <mail@joel-rose.de>
2023-03-11 22:41:38 +0100

    ci: add redis instance for tests

--- adapter/adapter_test.go
+++ adapter/adapter_test.go
@@ -28,7 +28,7 @@ func TestGetSet_Redigo(t *testing.T) {
 	GetSet(t, func(_ *testing.T) *redisstore.Store {
 		pool := &redis.Pool{
 			Dial: func() (redis.Conn, error) {
-				return redis.Dial("tcp", "localhost:6379")
+				return redis.Dial("tcp", "localhost:6379") // nolint: wrapcheck
 			},
 		}
 
@@ -37,6 +37,8 @@ func TestGetSet_Redigo(t *testing.T) {
 }
 
 func GetSet(t *testing.T, newStore storeFactory) {
+	t.Helper()
+
 	const (
 		sessionName = "session"
 		value       = "ok"
@@ -51,7 +53,8 @@ func GetSet(t *testing.T, newStore storeFactory) {
 		assert.NoError(t, err)
 
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(value))
+		_, err = w.Write([]byte(value))
+		assert.NoError(t, err)
 	}
 
 	get := func(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +68,8 @@ func GetSet(t *testing.T, newStore storeFactory) {
 		assert.NoError(t, err)
 
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(value))
+		_, err = w.Write([]byte(value))
+		assert.NoError(t, err)
 	}
 
 	delete := func(w http.ResponseWriter, r *http.Request) {
@@ -76,24 +80,28 @@ func GetSet(t *testing.T, newStore storeFactory) {
 		assert.NoError(t, err)
 
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(value))
+		_, err = w.Write([]byte(value))
+		assert.NoError(t, err)
 	}
 
 	res1 := httptest.NewRecorder()
-	req1, _ := http.NewRequest("GET", "/set", nil)
+	req1, _ := http.NewRequest(http.MethodGet, "/set", nil) // nolint:noctx
 	set(res1, req1)
 
 	res2 := httptest.NewRecorder()
-	req2, _ := http.NewRequest("GET", "/get", nil)
+	req2, _ := http.NewRequest(http.MethodGet, "/get", nil) // nolint:noctx
 	copyCookies(req2, res1)
 	get(res2, req2)
 
 	res3 := httptest.NewRecorder()
-	req3, _ := http.NewRequest("GET", "/delete", nil)
+	req3, _ := http.NewRequest(http.MethodGet, "/delete", nil) // nolint:noctx
 	copyCookies(req3, res2)
 	delete(res3, req3)
 
-	cookies := res3.Result().Cookies()
+	result := res3.Result()
+	defer result.Body.Close()
+
+	cookies := result.Cookies()
 	assert.Equal(t, 1, len(cookies))
 	assert.Equal(t, -1, cookies[0].MaxAge)
 	assert.Equal(t, "", cookies[0].Value)
